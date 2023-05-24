0503aa9df02bd7dc151a7586f7ca01b440813599
yoshitha-puchalapalli <114273229+yoshitha-puchalapalli@users.noreply.github.com>
2023-05-24 15:22:45 +0530

    Update kaprekar.py

--- kaprekar.py
+++ kaprekar.py
@@ -1,1 +1,11 @@
-#python
+n=int(input())
+a=n*n
+total=0
+while a>0:
+    dig=a%10
+    total=total+dig
+    a=a//10
+if(total==n):
+    print("Kaprekar Number")
+else:
+    print("Not A Kaprekar Number")
